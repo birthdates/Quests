d9b31d9ed3b4cd0171afdf6e45a006dc6dd11c3f
Liam <kubickiliam@gmail.com>
2024-01-10 09:27:49 -0500

    fix: scheduler errors

--- src/main/java/com/birthdates/quests/util/VerboseExecutor.java
+++ src/main/java/com/birthdates/quests/util/VerboseExecutor.java
@@ -3,7 +3,9 @@ package com.birthdates.quests.util;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,6 +20,16 @@ public class VerboseExecutor extends ScheduledThreadPoolExecutor {
         this.logger = logger;
     }
 
+    private Runnable wrapTask(Runnable task) {
+        return () -> {
+            try {
+                task.run();
+            } catch (Exception exception) {
+                logger.log(Level.SEVERE, "Failed to execute task", exception);
+            }
+        };
+    }
+
     /**
      * {@link java.util.concurrent.ThreadPoolExecutor#afterExecute(Runnable, Throwable)} is called with no exception always for {@link ScheduledThreadPoolExecutor}
      *
@@ -27,12 +39,11 @@ public class VerboseExecutor extends ScheduledThreadPoolExecutor {
     @NotNull
     @Override
     public Future<?> submit(@NotNull Runnable task) {
-        return super.submit(() -> {
-            try {
-                task.run();
-            } catch (Exception exception) {
-                logger.log(Level.SEVERE, "Failed to execute task", exception);
-            }
-        });
+        return super.submit(wrapTask(task));
+    }
+
+    @Override
+    public @NotNull ScheduledFuture<?> scheduleAtFixedRate(@NotNull Runnable command, long initialDelay, long period, @NotNull TimeUnit unit) {
+        return super.scheduleAtFixedRate(wrapTask(command), initialDelay, period, unit);
     }
 }
